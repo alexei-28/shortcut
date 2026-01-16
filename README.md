bbb03a6b7500ffc1febae50a8b1cad951d273172
Alexei <a_subscriber@mail.ru>
2026-01-16 22:33:40 +0200

    Init Shortcut project

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# shortcut
+# Shortcut - mentor platform
+  Site:
+  https://shortcut.education/
+
 Shortcut project
+
+Java: 25
+
+Gradle: 9.3
+
+Spring Boot: 4.0.1
---
+++ src/test/java/com/gmail/alexei28/shortcut/ShortcutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.alexei28.shortcut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortcutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
