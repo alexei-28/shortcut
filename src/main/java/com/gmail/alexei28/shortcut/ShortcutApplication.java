43ff56e313b7afeda707a34389573dd985bed0e8
Alexei <a_subscriber@mail.ru>
2026-01-28 11:02:05 +0200

    Kafka

--- src/main/java/com/gmail/alexei28/shortcut/ShortcutApplication.java
+++ src/main/java/com/gmail/alexei28/shortcut/ShortcutApplication.java
@@ -1,5 +1,6 @@
 package com.gmail.alexei28.shortcut;
 
+import com.gmail.alexei28.shortcut.kafka.producer.Producer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -7,11 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ShortcutApplication {
-    private static final Logger logger = LoggerFactory.getLogger(ShortcutApplication.class);
+  private static final Logger logger = LoggerFactory.getLogger(ShortcutApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(ShortcutApplication.class, args);
-        logger.info("Java version: {}, Java vendor: {}", System.getProperty("java.version"), System.getProperty("java.vendor"));
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(ShortcutApplication.class, args);
+    logger.info(
+        "Java version: {}, Java vendor: {}",
+        System.getProperty("java.version"),
+        System.getProperty("java.vendor"));
 
+    new Producer().sendSyncMessage();
+  }
 }
